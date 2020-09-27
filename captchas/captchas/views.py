d7453cb01b7a2d29f9ccda97514510b3bd6a7a58
Harmeet Singh <2018.harmeet.kathoda@ves.ac.in>
2020-09-27 22:15:30 +0530

    Frontend & Django Templates added

---
+++ captchas/captchas/views.py
@@ -0,0 +1,26 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+def home(request):
+    return render(request, 'home.html')
+
+def fun_math(request):
+    return render(request, 'funMath.html')
+
+def word_issue(request):
+    return render(request, 'wordIssue.html')
+
+def time_based(request):
+    return render(request, 'time.html')
+
+def no_captcha(request):
+    return render(request, 'nocaptcha.html')
+
+def invisible(request):
+    return render(request, 'invisible.html')
+
+def confident(request):
+    return render(request, 'confident.html')
+
+def slider(request):
+    return render(request, 'slider.html')
